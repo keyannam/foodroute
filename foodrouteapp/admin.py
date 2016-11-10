4a673c1473f8346d3f30950273a50ead997c3298
keyannam <keyannamayfield@gmail.com>
2016-11-09 22:33:53 -0500

    second commit

--- foodrouteapp/admin.py
+++ foodrouteapp/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from foodrouteapp.models import Restaurant
+from foodrouteapp.models import Restaurant, Customer, Driver
 
 admin.site.register(Restaurant)
+admin.site.register(Customer)
+admin.site.register(Driver)
